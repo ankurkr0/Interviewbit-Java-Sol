faaed289c97b0e3fb47e37fedf2110243915e43c
Ankur Kumar <70519427+ankurkr0@users.noreply.github.com>
2020-09-10 10:20:21 +0530

    Create Merge Two Sorted Lists II.java

---
+++ Two Pointer Problem/Merge Two Sorted Lists II.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public void merge(ArrayList<Integer> a, ArrayList<Integer> b) {
+        int i = 0;
+        int j = 0;
+
+        while(i<a.size() && j<b.size()){
+
+            if(a.get(i) >= b.get(j)){
+                a.add(i, b.get(j));
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+
+        while (j<b.size()){
+            a.add(b.get(j));
+            j++;
+        }
+    }
+}
