004cc4bd374cf262d38038355553160dc60ea841
Ankur Kumar <70519427+ankurkr0@users.noreply.github.com>
2020-09-17 09:59:05 +0530

    Create Reverse Linked List.java

---
+++ Linked List/Reverse Linked List.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode A) {
+        if(A==null || A.next == null) return A;
+        ListNode prev = null;
+        ListNode curr = A;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
